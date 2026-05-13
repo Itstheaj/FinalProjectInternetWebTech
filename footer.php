438cab38a66486f9413b3354e82d679fffa793b6
Itstheaj <119617133+Itstheaj@users.noreply.github.com>
2026-05-13 16:42:32 -0400

    Delete footer.php

--- footer.php
+++
@@ -1,7 +0,0 @@
-</main>
-    <footer>
-        <p>&copy; <?php echo date("Y"); ?> The Critic's Mirror | Powered by RAWG API</p>
-    </footer>
-    <script src="script.js"></script>
-</body>
-</html>
