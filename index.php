a8678af54abf9c1a713819c575fc5b9e04252cb3
Itstheaj <119617133+Itstheaj@users.noreply.github.com>
2026-05-13 16:42:16 -0400

    Delete index.php

--- index.php
+++
@@ -1,7 +0,0 @@
-<?php include 'header.php'; ?>
-<section class="hero">
-    <h1>Are you a mainstream gamer or a trendsetter?</h1>
-    <p>Compare your personal rankings against the world's harshest critics.</p>
-    <a href="video_game.php" class="btn">Start Your Analysis</a>
-</section>
-<?php include 'footer.php'; ?>
